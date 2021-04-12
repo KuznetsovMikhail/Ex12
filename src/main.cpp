94b4f93131905c343bd30d8477edebfab9458d90
KuznetsovMikhail <KuznetsovMikhail>
2021-04-12 21:18:57 +0300

    add all files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-// Copyright 2021 GHA Test Team
-#include "TimedDoor.h"
+// Copyright 2021 Kuznetsov Mikhail
 #include <iostream>
+#include <string>
+#include "TimedDoor.h"
 
 int main() {
-  TimedDoor tDoor(5);
-  tDoor.lock();
-  tDoor.unlock();
-
+  TimedDoor timedDoor(5);  // create door with 10-seconds timer
+  try {
+    timedDoor.unlock();
+  } catch (std::string notice) {
+    std::cout << notice << std::endl;  // close the door!
+  }
   return 0;
 }
