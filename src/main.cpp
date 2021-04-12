9f7c887b7fe96f844981c69451c81ed034447bac
KuznetsovMikhail <KuznetsovMikhail>
2021-04-12 21:44:03 +0300

    some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,25 @@
 #include "TimedDoor.h"
 
 int main() {
-  TimedDoor timedDoor(5);  // create door with 10-seconds timer
+  TimedDoor* timedDoor1 = new TimedDoor(5);  // create door with 10-seconds timer
   try {
-    timedDoor.unlock();
-  } catch (std::string notice) {
-    std::cout << notice << std::endl;  // close the door!
+    timedDoor1->unlock();
+    timedDoor1->throwState();
+  } catch (std::string notice1) {
+    std::cout << notice1 << std::endl;  // the door is opened!
+  }
+  TimedDoor* timedDoor2 = new TimedDoor(5);
+  try {
+    timedDoor2->unlock();
+  } catch (std::string notice2) {
+    std::cout << notice2 << std::endl;  // close the door!
+  }
+  TimedDoor* timedDoor3 = new TimedDoor(5);
+  try {
+    timedDoor3->lock();
+    timedDoor3->throwState();
+  } catch (std::string notice3) {
+    std::cout << notice3 << std::endl;  // the door is closed!
   }
   return 0;
 }
